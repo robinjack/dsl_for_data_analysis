525883969cc9366428429732b8e145e707be9a29
robinjack <robinmajack@gmail.com>
2020-07-22 15:53:43 +0100

    have added evaluator tests (although need to write out the DSL language) and updated the current status of the plan

--- dsl/test_evaluator.py
+++ dsl/test_evaluator.py
@@ -4,11 +4,21 @@ from semantic_model.data import Data
 
 meta = metamodel_from_file('dsl/analysis_flow.tx')
 
+"""
+There are two types of test in this test suite.
 
-# TODO: test evaluate
-# def evaluate(self, command):
-#     return self.function_table[command._tx_fqn](command)
+First is the functionality tests - can we do all of the 
+functions that are exposed by the language - i.e. 
+loading, analysing, manipulating, training, testing, reviewing.
 
+Second is the analysis flow
+    
+
+"""
+
+#####################################
+# Section one - testing functionality
+#####################################
 
 def test_load():
     model = meta.model_from_file('examples/test_load.analysis')
@@ -48,19 +58,44 @@ def test_basic_sum_and_product_expression():
 #     assert result.symbol_table["y"] == 100101
 
 """
+Secondly, is the analysis workflow. 
+These three tests will comprise the MVP of the product.
+There are three basic tests:
+1. The most basic analysis possible (look at the data, choose Y, and predict)
+2. The most basic manipulation possible 
+    look at the data
+    choose Y
+    predict Y
+    review results
+    manipulate the data
+    predict Y
+    compare results
+3. Two manipulations + rewind a manipulation to a previous snapshot
+    Same as above, but with two manipulations done,
+    Inspect the results + compare them
+"""
 
-def test_function_call():
-    model = meta.model_from_file('examples/test_function_call.analysis')
-    result = Evaluator(model)
-    assert False
-    
-    """
-
+#####################################
+# Section two - MVP tests
+#####################################
 
 def test_simple_program():
     model = meta.model_from_file('examples/test_program.analysis')
     result = Evaluator(model)
     result.run()
+#    TODO: add in assertion
+
+def test_simple_manipulation():
+    model = meta.model_from_file('examples/simple_manipulation.analysis')
+    result = Evaluator(model)
+    result.run()
+#    TODO: add in assertion
+
+def test_two_manipulations_and_rewind():
+    model = meta.model_from_file('examples/two_manipulations_and_rewind.analysis')
+    result = Evaluator(model)
+    result.run()
+#    TODO: add in assertion
 
 
 
